c4ae5d3f6f6cfc0eed0eb29b8cc1c6d381ce431f
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:38:42 +0000

    Create inscrieri_edit.php

---
+++ admin/inscrieri_edit.php
@@ -0,0 +1,99 @@
+<?php
+include 'head.php';
+$id = $_REQUEST['id'];
+include '../include/db_connect.php';
+$result = queryMysql ("SELECT * FROM inscrieri where id=$id");
+$row = mysqli_fetch_array($result);
+include_once '../include/db_rows_inscrieri.php';
+echo "<h1 style='text-align:center;margin-top:20px;'>Detalii solicitant $nume $prenume</h1>";
+?>
+<body>
+ 
+    
+<?php     
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $id = $_POST['id'];        
+        $nume = $_POST['nume'];
+        $prenume = $_POST['prenume'];
+        $telefon = $_POST['telefon'];
+        $email = $_POST['email'];        
+        $mesaj = $_POST['mesaj'];
+        $status = $_POST['status'];
+        $autor_status = $_POST['autor_status'];
+        
+
+        $result = queryMysql
+            ("
+            UPDATE inscrieri SET
+            nume='$nume',
+            prenume='$prenume',
+            telefon='$telefon',
+            email='$email',
+            mesaj='$mesaj',
+            status='$status',
+            autor_status='$autor_status'
+            WHERE id='$id';
+            ");
+        
+ 
+        
+        
+        header('location:inscrieri.php');
+
+    }
+?>       
+    
+    
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">   
+        <input type="hidden" name="id" value="<?php echo $id; ?>">
+    
+    
+        <label>Nume</label>
+        <br>
+        <input type="text" name="nume" value="<?php echo $nume; ?> ">
+        <hr>
+    
+        <label>Prenume</label>
+        <br>
+        <input type="text" name="prenume" value="<?php echo $prenume; ?>">
+        <hr>
+
+        <label>Telefon</label>
+        <br>
+        <input type="text" name="telefon" value="<?php echo $telefon; ?>">
+        <hr>
+    
+        <label>Email</label>
+        <br>
+        <input type="text" name="email" value="<?php echo $email; ?>">
+        <hr>
+    
+        <label>Evenimentul solicitat</label>
+        <br>
+        <input disabled type="text" name="evenimentID" value="<?php echo $evenimentID; ?>">
+        <hr>    
+
+        <label>Mesaj voluntar</label>
+        <br>
+        <textarea name="mesaj" value="<?php echo $mesaj; ?>"><?php echo $mesaj; ?></textarea>
+        <hr>
+    
+        <label>Status înscriere</label>
+        <br>
+            <select name="status">
+                <option selected value="<?php echo $status; ?>"><?php echo "--$status--"; ?></option>
+                <option value="in progress">In Progress</option>                
+                <option value="aprobat">Aprobat</option>
+                <option value="respins">Respins</option>
+            </select>
+    
+        <input type="text" placeholder="autor status" name="autor_status" value="<?php echo $autor_status; ?>">
+                
+        <br>
+        <br>
+    
+        <input type="submit" name="edit" value="Confirmă modificările" class="btn btn-success">
+    </form>        
+</body>
