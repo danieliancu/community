ea2f5c4df3d312d1394fe3e34ee2dfaad2963ff9
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:30:46 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php
+include 'header.php';
+$arhiva = "eveniment";
+
+?>
+<script>$(".menu a:nth-child(1)").css("color","red");</script>
+
+<div class="cover">
+    <div class="textCover">
+        <div class="eticheta">14 februarie - Valentine&#39;s Day</div>
+	    <div class="bigTitle">Cea mai romantică<br>petrecere în doi</div>
+        <div class="subtitle">Veniți împreună de ziua îndrăgostiților la petrecerea dedicată cuplurilor, în incinta <a href="https://www.google.com/maps/dir//Skylark+Hotel,+Aviation+Way,+Southend-on-Sea+SS2+6UN/@51.5697313,0.6142826,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x47d8d914cc2d2f8d:0x2c30a3cd0bbde45e!2m2!1d0.6843221!2d51.5697521?hl=en" target="_blank">hotelului Skylark</a>. Începând cu ora 18 AM, veți fi purtați într-o lume a muzicii și dansului romantic, cu dragoste și pasiune. Cuplurile vor fi centrul și sufletul acestui eveniment memorabil. Înscrierile se vor face în limita locurilor disponibile.</div>
+        <a href="page.php?id=3" class="buton">citește mai mult</a>
+    </div>    
+    <div class="backCover"></div>
+    <img src="pic/photography-of-couple-holding-hands-842546.jpg">
+</div>
+
+
+
+
+
+<div class="intro container-fluid">
+	<div class="row">
+	<div class="introNews col-md-4" id="introNews1">
+    	<div class="text">
+    		<div class="eticheta">Școala de Weekend</div>
+        	<div class="title">Comunitatea românilor din Southend On Sea va organiza săptămânal lecturi și jocuri educative în limba română, pentru cei mici</div>
+           <a href="#" class="buton btnIntroNews">citește</a>
+        </div>
+        <div class="backNews"></div>
+        <img src="pic/girls-on-desk-looking-at-notebook-159823.jpg">
+    </div>
+    
+	<div class="introNews col-md-4" id="introNews2">
+    	<div class="text">
+    		<div class="eticheta">8 Martie - Mărțișor</div>
+        	<div class="title">Ajută-ne să ducem tradiția mai departe! Ne învățăm copiii să facă mărțișoare pentru mame!</div>
+           <a href="#" class="buton">citește</a>
+        </div>
+        <div class="backNews"></div>
+        <img src="http://romaniaresponsabila.ro/wp-content/uploads/2015/02/martisoare-umanitare.jpg">
+    </div>
+    
+	<div class="introNews col-md-4" id="introNews3">
+    	<div class="text">
+    		<div class="eticheta">Caritate</div>
+        	<div class="title">Un român din Anglia a strâns 13.000 de lire pentru copiii nevoiași din România</div>
+           <a href="#" class="buton">citește</a>
+        </div>
+        <div class="backNews"></div>
+        <img src="https://www.romani-online.co.uk/wp-content/uploads/2016/11/tabara-copii-belgia-300x300.jpg">
+    </div>
+    
+    
+    </div>
+</div>
+
+
+
+
+<div class="who" id="who">
+	<div class="title" style="font-size:38px;line-height: 38px;">Cine suntem</div>
+    <div class="subtitle">Organization set up to provide help and raise money for those in need</div>
+    <div class="band"></div>
+    
+    <div class="whoWrap container-fluid">
+    	<div class="row">
+    	<div class="whoNews col-md-4">
+        	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0CDQn0jNd_xtHMNdwe9PULxpZOhk3QZAJ75e9XwaoCJSbBhZs&s">
+            <div class="title">Cu prieteni, pentru prieteni</div>
+            <div class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</div>            
+        </div>
+    	<div class="whoNews col-md-4">
+        	<img src="https://toursromania.files.wordpress.com/2014/12/calusarii1-620x300.jpg">
+            <div class="title">Muzica, dans, spectacol</div>
+            <div class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</div>            
+        </div> 
+    	<div class="whoNews col-md-4">
+        	<img src="https://cdn.hswstatic.com/gif/5-birthday-party-craft-ideas-360x240.jpg">
+            <div class="title">Familia si copiii</div>
+            <div class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam blandit hendrerit faucibus. Suspendisse hendrerit turpis dui, eget ultricies erat consequat ut. Sed ac velit iaculis, condimentum neqlu.</div>            
+        </div>
+     </div>   
+    </div>
+</div>
+
+
+
+
+<div class="story container-fluid" id="brexit">
+
+	<div class="row" style="z-index:1;position:relative;">
+    
+		<div class="movie col-md-6">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/58XpkO0ulaI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        
+    	<div class="stire col-md-6">
+        	<div class="eticheta">Dosar: Brexit</div>
+       		<div class="title">Care va fi soarta cetăţenilor UE din Marea Britanie după Brexit?</div>
+			<div class="band" style="margin:0px;"></div>
+    		<br>
+    		<div class="subtitle" style="color:white;">Deşi nu ne mai desparte multă vreme de momentul Brexit, o serie de chestiuni extrem de importante sunt încă neclare, între care situaţia cetăţenilor UE din Marea Britanie după ieşirea ţării din Uniunea Europeană. </div>   
+			<a href="#" class="buton">citeste mai mult</a>
+        </div>
+        
+    </div>
+    
+    <img src="pic/uk.jpg">
+    
+</div>
+
+    
+    
+
+<?php include 'arhiva.php'; ?>     
+<?php include 'footer.php'; ?>
