26f4935f57a81c0f77e99ee237e1a9ea3e1cd419
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:42:54 +0000

    Create db_rows_voluntari.php

---
+++ include/db_rows_voluntari.php
@@ -0,0 +1,11 @@
+<?php 
+$id = $row['id'];
+$nume = $row['nume'];
+$prenume = $row['prenume'];
+$telefon = $row['telefon'];
+$email = $row['email'];
+$mesaj = $row['mesaj'];
+$status = $row['status'];
+$autor_status = $row['autor_status'];
+$data = $row['data'];
+?>
