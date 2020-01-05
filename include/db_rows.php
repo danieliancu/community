a5ab9c204b64337927cae8711b59f3aff748c60c
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:42:07 +0000

    Create db_rows.php

---
+++ include/db_rows.php
@@ -0,0 +1,24 @@
+<?php 
+$id = $row['id'];
+$tip = $row['tip'];
+$data = $row['data'];
+$eveniment = $row['eveniment'];
+$trimitere = $row['trimitere'];
+$titlu = $row['titlu'];
+$text = $row['text'];
+$sapou = $row['sapou'];
+$foto = $row['foto'];
+$date = $row['date'];
+/*
+    $id = $row['id'];
+    $ordine = $row['ordine'];
+    $data = $row['data']
+    $eveniments = $row['eveniments'];
+    $trimitere = $row['trimitere'];
+    $titlu = $row['titlu'];
+    $sapou = $row['sapou'];
+    $text = $row['text'];
+    $foto = $row['foto'];
+    $date = $row['date'];
+*/    
+?>
