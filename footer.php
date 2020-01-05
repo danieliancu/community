d791fb41f96735bde82d234296626ab0361f21de
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:30:14 +0000

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+        
+    <img src="pic/logo_SRC_white.svg">
+    <div class="menuFooter">
+    	<a href="#">Evenimente</a>
+    	<a href="#">News</a>
+    	<a href="#">Caritate</a>
+    </div>    
+    
+    <div>
+        <a href="6454851546"><i class='far fa-envelope'></i></a>
+        <a href="info@abcd.com"><i class='fas fa-phone'></i></a>
+        <a href="facebook.com"><i class='fab fa-facebook'></i></a>        
+        <a href="twitter.com"><i class='fab fa-twitter-square'></i></a>
+        <a href="instagram.com"><i class='fab fa-instagram'></i></a>
+    </div>
+    
+</div>
+
+
+</body>
+</html>
