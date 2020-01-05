61c367ea8795379b80cc316a36cf20cffbb88908
danieliancu <57058068+danieliancu@users.noreply.github.com>
2020-01-05 17:42:38 +0000

    Create db_rows_inscrieri.php

---
+++ include/db_rows_inscrieri.php
@@ -0,0 +1,12 @@
+<?php 
+$id = $row['id'];
+$nume = $row['nume'];
+$prenume = $row['prenume'];
+$telefon = $row['telefon'];
+$email = $row['email'];
+$mesaj = $row['mesaj'];
+$status = $row['status'];
+$autor_status = $row['autor_status'];
+$data = $row['data'];
+$evenimentID = $row['evenimentID'];
+?>
